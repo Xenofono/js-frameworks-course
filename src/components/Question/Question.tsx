8c2d68cb478d152ebbeb431b323c29257934dda5
Kristoffer Näsström <kristoffer.nasstrom@gmail.com>
2020-08-31 10:54:00 +0200

    simplified component by moving state to parent

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,24 +1,72 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, MouseEvent, useState } from "react";
+import { List, ListItem, ListItemText, Divider } from "@material-ui/core";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 
 type QuestionProps = {
   category: string;
   question: string;
+  options: string[];
+  goToNextQuestion: Function;
+  clicked: boolean;
   correctAnswer: string;
-  incorrectAnswers: string[];
 };
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      backgroundColor: theme.palette.background.paper,
+    },
+    correct: {
+      backgroundColor: "green",
+    },
+    incorrect: {
+      backgroundColor: "red",
+    },
+  })
+);
 
 const Question: FunctionComponent<QuestionProps> = ({
   category,
   question,
-  correctAnswer,
-  incorrectAnswers,
+  options,
+  goToNextQuestion,
+  clicked,
+  correctAnswer
 }) => {
+  const classes = useStyles();
 
-    // incorrectAnswers.push(correctAnswer)
-    
-return <div>
-    <h3>{category}</h3>
-    <h4>{question}</h4></div>;
+
+
+  const handleClick = (answer: String) => {
+
+      goToNextQuestion(answer);
+
+  };
+
+  const answersList = options.map((answer) => {
+    return (
+      <ListItem
+        key={answer}
+        onClick={(e: MouseEvent) => handleClick(answer)}
+        divider
+        selected={clicked}
+        className={answer === correctAnswer ? classes.correct : classes.incorrect}>
+        <ListItemText primary={answer} />
+      </ListItem>
+    );
+  });
+
+  return (
+    <div className={classes.root}>
+      <h3>{category}</h3>
+      <h4>{question}</h4>
+      <List component="nav" aria-label="">
+        {answersList}
+      </List>
+    </div>
+  );
 };
 
 export default Question;
