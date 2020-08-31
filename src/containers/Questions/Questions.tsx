628129fb511de84bbde7f7bdbf4bb8bf22109041
Kristoffer Näsström <kristoffer.nasstrom@gmail.com>
2020-08-31 10:55:45 +0200

    added logic to display if user chose correctly before rendering next question

--- src/containers/Questions/Questions.tsx
+++ src/containers/Questions/Questions.tsx
@@ -1,56 +1,73 @@
-import React, {FunctionComponent, useEffect, useState} from 'react';
-import Question from '../../components/Question/Question'
+import React, { FunctionComponent, useEffect, useState } from "react";
+import Question from "../../components/Question/Question";
 
-import {connect} from 'react-redux'
-import {useHistory} from 'react-router-dom'
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-import { CircularProgress } from '@material-ui/core';
+import { CircularProgress } from "@material-ui/core";
 
+import QuestionModel from '../../models/QuestionModel'
 
-export interface QuestionModel {
-    category: string,
-    question: string;
-  correctAnswer: string;
-  incorrectAnswers: string[];
-}
 
 interface QuestionsProps {
-    questions: QuestionModel[],
-    loading: boolean
+  questions: QuestionModel[];
+  loading: boolean;
 }
 
-const Questions: FunctionComponent<QuestionsProps> = ({questions, loading}) => {
-    const [questionIndex, setQuestionIndex] = useState(0)
 
-    const history = useHistory()
-    if(!loading && !questions){
-        history.replace("/")
+const Questions: FunctionComponent<QuestionsProps> = ({
+  questions,
+  loading,
+}) => {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [clicked, setClicked] = useState(true)
+  const history = useHistory();
+
+  const goToNextQuestion = (answer: string) => {
+    setClicked(false)
+    const currentQuestion = questions[questionIndex];
+    if (answer === currentQuestion.correctAnswer) {
+      console.log("rätt svar!");
+    } else {
+      console.log("fel!");
     }
+    setTimeout(() => {
+      const nextIndex = questionIndex+1;
+      if(nextIndex < questions.length){
+      setQuestionIndex(questionIndex + 1);
+  
+      }
+      setClicked(true)
+    },1500)
 
-    let toShow = <CircularProgress></CircularProgress>
-    if(!loading && questions){
-        const nextQuestion = questions[questionIndex]
-        toShow = <Question category={nextQuestion.category}
+  }
+
+  if (!loading && !questions) {
+    history.replace("/");
+  }
+
+  let toShow = <CircularProgress></CircularProgress>;
+  if (!loading && questions) {
+    const nextQuestion = questions[questionIndex];
+    console.log("QUESTIONSSSS: ", nextQuestion)
+    toShow = (
+      <Question
+        category={nextQuestion.category}
         question={nextQuestion.question}
+        options={nextQuestion.options}
+        goToNextQuestion={goToNextQuestion}
         correctAnswer={nextQuestion.correctAnswer}
-        incorrectAnswers={nextQuestion.incorrectAnswers}></Question>
+        clicked={clicked}
+        ></Question>
+    );
+  }
 
-    }
-
-    
-
-    return (
-        <div>
-            {toShow}
-        </div>
-    )
-}
-
-
+  return <div>{toShow}</div>;
+};
 
 const mapStateToProps = (state: any) => ({
-    questions: state.currentQuiz,
-    loading: state.loading
-})
+  questions: state.currentQuiz,
+  loading: state.loading,
+});
 
-export default connect(mapStateToProps)(Questions)
+export default connect(mapStateToProps)(Questions);
